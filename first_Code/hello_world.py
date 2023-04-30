7d32a26b001d5b5624d7f300582bf9c5c2b14105
Laurizimdars <131323549+Laurizimdars@users.noreply.github.com>
2023-04-30 17:09:58 +0000

    Meu primeiro commit

---
+++ first_Code/hello_world.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+def print_hello_world():
+    """It print 'Hello World!' to the console"""
+    print("Hello World")
+    
+   
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 # -*- coding: utf-8 -*-
-print("Hello World")
+
+from first_Code.hello_world import print_hello_world
+
+print_hello_world()
